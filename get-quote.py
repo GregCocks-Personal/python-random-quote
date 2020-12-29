495e557a93c4ac8f3fc1ea404636e4e865d33a06
GregCocks-Personal <skiwi@spamcop.net>
2020-12-28 19:27:11 -0600

    pick a randon quote from the lst

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,20 @@
+import random
+
 def primary():
 
-  print("Keep it logically awesome.")
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
 
-  #print(quotes)
+  print('{}) {}'.format(rnd,quotes[rnd]))
+
+  # intCounter = 0
+  # while intCounter < len(quotes):
+      # print('{}) {}'.format(intCounter+1,quotes[intCounter]))
+      # intCounter +=1
 
 if __name__== "__main__":
   primary()
